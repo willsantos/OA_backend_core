409376a85e077d97dda04e1b9be1789db9e126dd
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-08-14 15:30:45 -0300

    :construction: feat/Criando Entidade Usuário e context

---
+++ OA_Core.Domain/Entities/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA_Core.Domain.Entities
+{
+    public class Usuario
+    {
+        public Guid id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public DateOnly data_nascimento { get; set; }
+        public string telefone { get; set; }
+        public string endereco { get; set; }
+    }
+}
---
+++ OA_Core.Repository/Context/OA_CoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace OA_Core.Repository.Context
+{
+    public class OA_CoreContext : DbContext
+    {
+        public OA_CoreContext(DbContextOptions options) : base(options)
+        {
+        }
+       // public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
