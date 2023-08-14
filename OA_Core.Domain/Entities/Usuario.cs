7d85f5a52ec6bec32afa81acd27d271d72d1c6c8
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-08-14 15:33:20 -0300

    :construction: feat/Criando Entidade Usuário e context

--- OA_Core.Domain/Entities/Usuario.cs
+++ OA_Core.Domain/Entities/Usuario.cs
@@ -8,12 +8,12 @@ namespace OA_Core.Domain.Entities
 {
     public class Usuario
     {
-        public Guid id { get; set; }
-        public string nome { get; set; }
-        public string email { get; set; }
-        public string senha { get; set; }
-        public DateOnly data_nascimento { get; set; }
-        public string telefone { get; set; }
-        public string endereco { get; set; }
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime Data_Nascimento { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
     }
 }
