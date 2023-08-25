077d3d784c3127f8df4fa790ed699ac017a23eb2
unknown <pedro.silva@rarolabs.com.br>
2023-08-25 07:16:18 -0300

    fix: fix atributos usuario entitie e contratos

--- OA_Core.Domain/Contracts/Response/UsuarioResponse.cs
+++ OA_Core.Domain/Contracts/Response/UsuarioResponse.cs
@@ -4,8 +4,8 @@
     {
         public string Nome { get; set; }
         public string Email { get; set; }
-        public DateOnly DataNascimento { get; set; }
-        public string Telofone { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
         public string Endereco { get; set; }
     }
 }
--- OA_Core.Domain/Entities/Usuario.cs
+++ OA_Core.Domain/Entities/Usuario.cs
@@ -5,8 +5,8 @@
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
-        public DateOnly DataNascimento { get; set; }
-        public string Telofone { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
         public string Endereco { get; set; }
     }
 }
