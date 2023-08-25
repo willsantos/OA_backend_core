f94f41ac1b63370bd1dcd7bd21b90909069f5960
unknown <pedro.silva@rarolabs.com.br>
2023-08-25 07:17:51 -0300

    feat: add interfaces usuarioService e usuarioRepository

---
+++ OA_Core.Domain/Interfaces/Service/IUsuarioService.cs
@@ -0,0 +1,14 @@
+﻿using OA_Core.Domain.Contracts.Response;
+using OA_Core.Domain.Entities;
+
+namespace OA_Core.Domain.Interfaces.Service
+{
+    public interface IUsuarioService
+    {
+        Task<Guid> PostUsuarioAsync(Usuario usuario);
+        Task PutUsuarioAsync(Guid id, Usuario usuario);
+        Task DeleteUsuarioAsync(Guid id);
+        Task<Usuario> GetUsuarioByIdAsync(Guid id);
+        Task<IEnumerable<Usuario>> GetAllUsuariosAsync(int page, int rows);
+    }
+}
