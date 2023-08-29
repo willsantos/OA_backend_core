fb57ca947160d054b691ae88fb830e5f9b80290d
unknown <pedro.silva@rarolabs.com.br>
2023-08-29 17:44:33 -0300

    chore: filtros config

--- OA_Core.Api/Program.cs
+++ OA_Core.Api/Program.cs
@@ -1,32 +1,56 @@
+using OA_Core.Api.Filters;
 using OA_Core.Domain.Config;
+using OA_Core.Domain.Interfaces.Notifications;
 using OA_Core.Domain.Interfaces.Repository;
 using OA_Core.Domain.Interfaces.Service;
+using OA_Core.Domain.Notifications;
 using OA_Core.Repository.Context;
 using OA_Core.Repository.Repositories;
 using OA_Core.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
+
+#region AutoMapper
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+#endregion
+
+#region appConfig
 
 var appConfig = builder.Configuration.GetSection(nameof(AppConfig)).Get<AppConfig>();
 builder.Services.AddSingleton(appConfig);
 
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+#endregion
+
+#region Filtros
+
+builder.Services.AddMvc(options =>
+{
+    options.Filters.Add<NotificatonFilter>();
+    options.Filters.Add<ExceptionFilter>();
+});
+
+#endregion
+
+#region Injecao de dependencias
 
 builder.Services.AddScoped<DapperDbConnection>();
+builder.Services.AddScoped<INotificador, Notificador>();
 builder.Services.AddScoped<IUsuarioService, UsuarioService>();
 builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+#endregion
 
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
