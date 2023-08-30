0e91ab695f436052251aa6edf6aeb48a1f1576b5
Edivandro <edileal1@gmail.com>
2023-08-30 11:28:10 -0300

    AlunoRepository

---
+++ OA_Core.Repository/Repositories/AlunoRepository.cs
@@ -0,0 +1,13 @@
+﻿using OA_Core.Domain.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA_Core.Repository.Repositories
+{
+    public class AlunoRepository : IAlunoRepository
+    {
+    }
+}
