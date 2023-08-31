aea0b237bdbda0fa231a4ff79dcf16bc9bd43f69
Edivandro <edileal1@gmail.com>
2023-08-31 14:40:13 -0300

    feat(crud): construtores entidade aluno

--- OA_Core.Domain/Entities/Aluno.cs
+++ OA_Core.Domain/Entities/Aluno.cs
@@ -1,7 +1,25 @@
-﻿namespace OA_Core.Domain.Entities
+﻿using OA_Core.Domain.Validations;
+
+namespace OA_Core.Domain.Entities
 {
     public class Aluno : Entidade
     {
+
+        public Aluno(Guid usuarioId)
+        {
+            Id = Guid.NewGuid();
+            UsuarioId = usuarioId;
+            DataCriacao = DateTime.Now;
+            Validate(this, new AlunoValidator());
+        }
+        public Aluno(Guid id, DateTime dataCriacao, DateTime dataAlteracao, DateTime dataDelecao)
+        {
+            Id = id;
+            DataCriacao = dataCriacao;
+            DataAlteracao = dataAlteracao;
+            DataDelecao = dataDelecao;
+        }
+
         public Guid UsuarioId { get; set; }
     }
 }
