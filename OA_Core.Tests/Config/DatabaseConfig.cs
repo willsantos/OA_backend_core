b85502656ffe22c10a682922ff386b54d85dd36b
Alisson Demys <Alisson_demys@hotmail.com>
2023-09-05 14:02:11 -0300

    Iniciando configuração de ambiente de testes

---
+++ OA_Core.Tests/Config/DatabaseConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OA_Core.Repository.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA_Core.Tests.Config
+{
+    public class DatabaseConfig : IDisposable
+    {
+        public readonly CoreDbContext context;
+        public DatabaseConfig() 
+        {
+            var builder = new DbContextOptionsBuilder<CoreDbContext>();
+            builder.UseInMemoryDatabase(databaseName: "CoreDbInMemory");
+
+            var dbContextOptions = builder.Options;
+            context = new CoreDbContext(dbContextOptions);
+            context.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            context.Database.EnsureDeleted();
+        }
+    }
+}
