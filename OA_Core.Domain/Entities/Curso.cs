0a48db111cc8fe4de7c5176bd687acc4d14658d7
Alisson Demys <Alisson_demys@hotmail.com>
2023-09-05 11:35:55 -0300

    Implementa Validator e Construtor

--- OA_Core.Domain/Entities/Curso.cs
+++ OA_Core.Domain/Entities/Curso.cs
@@ -1,7 +1,21 @@
-﻿namespace OA_Core.Domain.Entities
+﻿using OA_Core.Domain.Validations;
+
+namespace OA_Core.Domain.Entities
 {
     public class Curso : Entidade
     {
+        public Curso(string nome, string descricao, string categoria, string preRequisito, double preco, Guid professorId)
+        {
+            Id = Guid.NewGuid();
+            Nome = nome;
+            Descricao = descricao;
+            Categoria = categoria;
+            PreRequisito = preRequisito;
+            Preco = preco;
+            ProfessorId = professorId;
+            DataCriacao = DateTime.Now;
+            Validate(this, new CursoValidator());
+        }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public string Categoria { get; set; }
