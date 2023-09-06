a34ac16af748417f74c54735a1b59c1d8e1cbb91
Alisson Demys <Alisson_demys@hotmail.com>
2023-09-06 11:16:27 -0300

    Remove codigos não funcionais

---
+++ OA_Core.Tests/Repository/CursoRepositoryTest.cs
@@ -0,0 +1,26 @@
+﻿using AutoFixture;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+using OA_Core.Domain.Entities;
+using OA_Core.Repository.Context;
+using OA_Core.Repository.Repositories;
+using OA_Core.Tests.Config;
+
+namespace OA_Core.Tests.Repository
+{
+    public class CursoRepositoryTest
+    {
+        private readonly Fixture _fixture;
+        private readonly CoreDbContext _context;
+
+        public CursoRepositoryTest()
+        {
+            _fixture = FixtureConfig.GetFixture();
+        }
+
+        [Fact]
+        public void TesteCriarCurso()
+        {
+        }
+    }
+}
