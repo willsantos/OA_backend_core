643d96b1724df2d2417000f15de84e129e2fa4eb
Alisson Demys <Alisson_demys@hotmail.com>
2023-09-09 22:07:32 -0300

    Adiciona AulaService

---
+++ OA_Core.Domain/Interfaces/Service/IAulaService.cs
@@ -0,0 +1,14 @@
+﻿using OA_Core.Domain.Contracts.Request;
+using OA_Core.Domain.Contracts.Response;
+
+namespace OA_Core.Domain.Interfaces.Service
+{
+    public interface IAulaService
+    {
+        Task<Guid> PostAulaAsync(AulaRequest aulaRequest);
+        Task PutAulaAsync(Guid id, AulaRequestPut aulaRequest);
+        Task DeleteAulaAsync(Guid id);
+        Task<AulaResponse> GetAulaByIdAsync(Guid id);
+        Task<IEnumerable<AulaResponse>> GetAllAulasAsync(int page, int rows);
+    }
+}
