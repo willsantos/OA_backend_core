7b987a0737514aaa7a4d9a77e12a3d994476b32e
Alisson Demys <Alisson_demys@hotmail.com>
2023-09-09 22:15:18 -0300

    adiciona requestput

---
+++ OA_Core.Domain/Contracts/Request/AulaRequestPut.cs
@@ -0,0 +1,12 @@
+﻿namespace OA_Core.Domain.Contracts.Request
+{
+    public class AulaRequestPut
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public string Caminho { get; set; }
+        public string Tipo { get; set; }
+        public int Duracao { get; set; }
+        public int Ordem { get; set; }
+    }
+}
