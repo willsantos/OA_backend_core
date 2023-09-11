0c2105818f0de7245d10861df093f7e9787b13bd
Wilson Santos <contato@wilsonsantos.net>
2023-09-11 00:09:17 -0300

    test:Adiciona testes do Value Object CPF

---
+++ OA_Core.Domain/ValueObjects/Cpf.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA_Core.Domain.ValueObjects
+{
+    public class Cpf
+    {
+        public static IEnumerable<char> Exibir(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IEnumerable<char> ExibirFormatado(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool Verificar(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
