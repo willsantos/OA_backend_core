610e9e811bc0d64f7883847c1f4bd19759c0b646
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-09-12 14:16:39 -0300

    :test_tube: test/testes de busca de professor por id e busca por todos os professores

--- OA_Core.Tests/Controller/ProfessorControllerTest.cs
+++ OA_Core.Tests/Controller/ProfessorControllerTest.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
 using OA_Core.Api.Controllers;
 using OA_Core.Domain.Contracts.Request;
+using OA_Core.Domain.Contracts.Response;
 using OA_Core.Domain.Entities;
 using OA_Core.Domain.Interfaces.Service;
 using OA_Core.Tests.Config;
@@ -49,6 +50,45 @@ namespace OA_Core.Tests.Controller
 
 			Assert.Equal(StatusCodes.Status201Created, actionResult.StatusCode);
 			Assert.Equal(entity.Id, actionResult.Value);
+		}
+		[Fact(DisplayName = "Busca todos os professores")]
+		public async Task GetAllProfessorAsync()
+		{
+			var professorController = new ProfessorController(_service);
+
+			var entities = _fixture.Create<List<ProfessorResponse>>();
+
+			int page = 0;
+			int rows = 10;
+
+			_service.GetAllProfessoresAsync(page, rows).Returns(entities);
+
+			var controllerResult = await professorController.GetAllProfessorAsync(page, rows);
+
+			Assert.IsType<OkObjectResult>(controllerResult.Result);
+
+			var resultValue = (controllerResult.Result as OkObjectResult).Value as PaginationResponse<ProfessorResponse>;
+
+			Assert.Equal(entities, resultValue.Resultado);
+		}
+
+		[Fact(DisplayName = "Busca professor por ID")]
+		public async Task GetProfessorByIdAsync()
+		{
+			var professorController = new ProfessorController(_service);
+
+			var entity = _fixture.Create<ProfessorResponse>();
+			Guid id = Guid.NewGuid();
+
+			_service.GetProfessorByIdAsync(id).Returns(entity);
+
+			var controllerResult = await professorController.GetProfessorByIdAsync(id);
+
+			Assert.IsType<OkObjectResult>(controllerResult.Result);
+
+			var resultValue = (controllerResult.Result as OkObjectResult).Value as ProfessorResponse;
+
+			Assert.Equal(entity, resultValue);
 		}
 	}
 	
