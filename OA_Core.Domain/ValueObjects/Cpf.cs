90e2c380b5ae1178591c7ef37c0e1991f10abb78
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-09-13 14:28:40 -0300

    :construction: feat/ValueObjectCpf

--- OA_Core.Domain/ValueObjects/Cpf.cs
+++ OA_Core.Domain/ValueObjects/Cpf.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OA_Core.Domain.ValueObjects
+﻿namespace OA_Core.Domain.ValueObjects
 {
     public class Cpf
     {
@@ -12,22 +6,69 @@ namespace OA_Core.Domain.ValueObjects
 
         public Cpf(string registro)
         {
-            Registro = registro;
-        }
+			if (!Verificar(registro))
+			{
+				throw new ArgumentException("CPF inválido.", nameof(registro));
+			}
 
-        public string Exibir()
-        {
-            throw new NotImplementedException();
+			Registro = registro;
         }
+		public string Formatted => ExibirFormatado(Registro);		
+		//Sobrescreve método toString() formatando para cpf
+		public override string ToString() => Formatted;
+		//Exibir() exibe o valor de CPF 
+		public string Exibir(string cpf)
+        {
+			cpf = new string(cpf.Where(char.IsDigit).ToArray());
+			return $"{cpf.Substring(0, 3)}{cpf.Substring(3, 3)}{cpf.Substring(6, 3)}{cpf.Substring(9)}";
+		}
 
-        public string ExibirFormatado()
+		//ExibirFormatado() exibe o valor de CPF formatado com pontuação e hífen.
+		public string ExibirFormatado(string cpf)
         {
-            throw new NotImplementedException();
-        }
+			//remove caracteres nao numericos
+			cpf = new string(cpf.Where(char.IsDigit).ToArray());
+			return $"{cpf.Substring(0, 3)}.{cpf.Substring(3, 3)}.{cpf.Substring(6, 3)}-{cpf.Substring(9)}";
+		}
 
-        public bool Verificar()
+		//Verificar() (verifica se o CPF é valido) 
+		public bool Verificar(string cpf)
         {
-            throw new NotImplementedException();
-        }
-    }
+			if (string.IsNullOrWhiteSpace(cpf))
+			{
+				return false;
+			}
+			
+			cpf = Exibir(cpf);
+		
+			if (cpf.Length != 11)
+			{
+				return false;
+			}
+
+			// Se todos os digitos forem iguais, retorna falso
+			if (cpf.Distinct().Count() == 1)
+			{
+				return false;
+			}
+
+			// Cálculo do dígito verificador
+			int[] numeros = cpf.Select(c => int.Parse(c.ToString())).ToArray();
+			int soma1 = 0, soma2 = 0;
+
+			for (int i = 0; i < 9; i++)
+			{
+				soma1 += numeros[i] * (10 - i);
+				soma2 += numeros[i] * (11 - i);
+			}
+
+			int digito1 = (soma1 * 10) % 11;
+			int digito2 = (soma2 * 10) % 11;
+
+			if (digito1 == 10) digito1 = 0;
+			if (digito2 == 10) digito2 = 0;
+
+			return digito1 == numeros[9] && digito2 == numeros[10];
+		}	
+	}
 }
