b534fe78990d049c50a250e38946c2b4f28f86f5
Wilson Santos <contato@wilsonsantos.net>
2023-09-14 18:05:49 -0300

    test: Add bogus e Cria Fixture de usuario

---
+++ OA_Core.Tests/Config/UsuarioFixture.cs
@@ -0,0 +1,62 @@
+﻿using Bogus;
+using OA_Core.Domain.Contracts.Request;
+using OA_Core.Domain.Entities;
+
+namespace OA_Core.Tests.Config
+{
+
+	[CollectionDefinition(nameof(UsuarioBogusCollection))]
+	public class UsuarioBogusCollection : ICollectionFixture<UsuarioFixture>
+	{ }
+
+	public class UsuarioFixture : IDisposable
+	{
+
+		public Usuario GerarUsuario()
+		{
+			var usuario = GerarUsuarios(1, true).FirstOrDefault();
+
+			return usuario is null ? throw new Exception("Falha ao gerar usuario com Bogus") : usuario;
+		}
+
+		public UsuarioRequest GerarUsuarioRequest()
+		{
+			var usuario = GerarUsuarios(1, true).FirstOrDefault() ?? throw new Exception("Falha ao gerar usuario com Bogus");
+
+			var usuarioRequest = new UsuarioRequest
+			{
+				Nome = usuario.Nome,
+				Email = usuario.Email,
+				Senha = usuario.Senha,
+				DataNascimento = usuario.DataNascimento,
+				Telefone = usuario.Telefone,
+				Endereco = usuario.Endereco
+
+			};
+
+			return usuarioRequest;
+
+		}
+
+		public IEnumerable<Usuario> GerarUsuarios(int quantidade, bool ativo)
+		{
+			var usuarios = new Faker<Usuario>("pt_BR")
+				.CustomInstantiator(prop => new Usuario(
+					prop.Name.FirstName(),
+					prop.Internet.Email(),
+					prop.Internet.Password(),
+					prop.Date.Past(80, DateTime.Now.AddYears(-18)),
+					prop.Phone.PhoneNumber(),
+					prop.Address.StreetName()));
+
+
+			return usuarios.Generate(quantidade);
+
+		}
+
+		public void Dispose()
+		{
+			
+		}
+	}
+}
