9cef18f2d689152cd95dbca9d5ce9b4881fb966a
Edivandro <edileal1@gmail.com>
2023-09-18 14:30:04 -0300

    feat(refact): criação da base repository

---
+++ OA_Core.Domain/Interfaces/Repository/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace OA_Core.Domain.Interfaces.Repository
+{
+	public interface IBaseRepository<T> where T : class
+	{
+		Task<T> ObterPorIdAsync(Guid id);
+		Task<T> ObterAsync(Expression<Func<T, bool>> expression);
+		Task<IEnumerable<T>> ObterTodosAsync(Expression<Func<T, bool>> expression);
+		Task AdicionarAsync(T item);
+		Task RemoverAsync(T item);
+		Task EditarAsync(T item);
+	}
+}
---
+++ OA_Core.Repository/Repositories/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OA_Core.Domain.Interfaces.Repository;
+using OA_Core.Repository.Context;
+using System.Linq.Expressions;
+
+namespace OA_Core.Repository.Repositories
+{
+	public class BaseRepository<T> : IBaseRepository<T> where T : class
+	{
+		private readonly CoreDbContext _coreDbContext;
+
+		public BaseRepository(CoreDbContext coreDbContext)
+		{
+			_coreDbContext = coreDbContext;
+		}
+
+		public async Task AdicionarAsync(T item)
+		{
+			await _coreDbContext.Set<T>().AddAsync(item);
+			await _coreDbContext.SaveChangesAsync();
+		}
+
+		public async Task EditarAsync(T item)
+		{
+			_coreDbContext.Set<T>().Update(item);
+			await _coreDbContext.SaveChangesAsync();
+		}
+
+		public async Task<T> ObterAsync(Expression<Func<T, bool>> expression)
+		{
+			return await _coreDbContext.Set<T>().AsNoTracking().FirstOrDefaultAsync(expression);
+		}
+
+		public async Task<T> ObterPorIdAsync(Guid id)
+		{
+			return await _coreDbContext.Set<T>().FindAsync(id);
+		}
+
+		public async Task<IEnumerable<T>> ObterTodosAsync(Expression<Func<T, bool>> expression)
+		{
+			return await _coreDbContext.Set<T>().Where(expression).ToListAsync();
+		}
+
+		public async Task RemoverAsync(T item)
+		{
+			_coreDbContext.Set<T>().Remove(item);
+			await _coreDbContext.SaveChangesAsync();
+		}
+	}
+}
