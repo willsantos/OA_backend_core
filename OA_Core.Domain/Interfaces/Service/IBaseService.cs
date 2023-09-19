05c084d85343556320cd103f7ffb46ae5be7f515
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-09-19 09:54:14 -0300

    :construction: fix/construindi interface e base service

---
+++ OA_Core.Domain/Interfaces/Service/IBaseService.cs
@@ -0,0 +1,12 @@
+﻿namespace OA_Core.Domain.Interfaces.Service
+{
+	public interface IBaseService<TResponse> where TResponse : class
+	{
+		Task<TResponse> ObterPorIdAsync<TRequest>(Guid id);
+		Task<TResponse> ObterAsync<TRequest>(int page, int rows);
+		Task<IEnumerable<T>> ObterTodosAsync(string parametros);
+		Task <Guid>AdicionarAsync<TRequest>(TRequest item);
+		Task RemoverAsync<TRequest>(Guid id);
+		Task EditarAsync<TRequest>(Guid id, TRequest item);
+	}
+}
