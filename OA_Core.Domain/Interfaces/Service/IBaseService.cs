46acbbea8504a1702319ef3cb6710c6dd7a1a250
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-09-20 10:00:02 -0300

    :construction: feat/apagando base service e mantendo apenas interface

--- OA_Core.Domain/Interfaces/Service/IBaseService.cs
+++ OA_Core.Domain/Interfaces/Service/IBaseService.cs
@@ -1,12 +1,12 @@
 ﻿namespace OA_Core.Domain.Interfaces.Service
 {
-	public interface IBaseService<TResponse> where TResponse : class
+	public interface IBaseService<TResponse, TRequest, T> where TResponse : class where TRequest : class where T : class
 	{
-		Task<TResponse> ObterPorIdAsync<TRequest>(Guid id);
-		Task<TResponse> ObterAsync<TRequest>(int page, int rows);
+		Task<TResponse> ObterPorIdAsync(Guid id);
+		Task<TResponse> ObterAsync(int page, int rows);
 		Task<IEnumerable<TResponse>> ObterTodosAsync(string parametros);
-		Task <Guid>AdicionarAsync<TRequest>(TRequest item);
-		Task RemoverAsync<TRequest>(Guid id);
-		Task EditarAsync<TRequest>(Guid id, TRequest item);
+		Task <Guid>AdicionarAsync(TRequest item);
+		Task RemoverAsync(Guid id);
+		Task EditarAsync<TRequestPut>(Guid id, TRequest item);
 	}
 }
