66afa8ed1b1409b6d88cfa30dc7a14e9bc345d23
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-09-20 10:15:32 -0300

    :construction: feat/ alterando IBaseService. Acrescentando métodos que recebem parametros

--- OA_Core.Domain/Interfaces/Service/IBaseService.cs
+++ OA_Core.Domain/Interfaces/Service/IBaseService.cs
@@ -5,7 +5,7 @@ namespace OA_Core.Domain.Interfaces.Service
 	public interface IBaseService<TResponse> where TResponse : class
 	{
 		Task<TResponse> ObterPorIdAsync(Guid id);
-		Task<TResponse> ObterTodosAsync(int page, int rows);
+		Task<IEnumerable<TResponse>> ObterTodosAsync(int page, int rows);
 		Task<TResponse> ObterAsync(string parametros);
 		Task<IEnumerable<TResponse>> ObterTodosAsync(string expression);
 		Task <Guid>AdicionarAsync<TRequest>(TRequest item);
