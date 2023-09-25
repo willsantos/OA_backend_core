2ea1502fcfec136a0e606d92d0b9e8ec30c5aa86
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-09-25 14:48:05 -0300

    :construction: feat/ retirando usyngs desnecessários

--- OA_Core.Domain/Interfaces/Repository/IAlunoRepository.cs
+++ OA_Core.Domain/Interfaces/Repository/IAlunoRepository.cs
@@ -1,9 +1,5 @@
-﻿    using OA_Core.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OA_Core.Domain.Entities;
+
 
 namespace OA_Core.Domain.Interfaces.Repository
 {
