35fa514ec84527117eb80a48fed41fa9049a9ba1
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-09-25 12:29:12 -0300

    :construction: feat/ criando entidade assintatura

---
+++ OA_Core.Domain/Entities/Assinatura.cs
@@ -0,0 +1,44 @@
+﻿using OA_Core.Domain.Utils;
+using OA_Core.Domain.Validations;
+
+namespace OA_Core.Domain.Entities
+{
+	public class Assinatura : Entidade
+	{
+		public Assinatura(Guid usuarioId, AssinaturaTipoEnum tipo, AssinaturaStatusEnum status, DateTime dataAtivacao, DateTime dataVencimento, DateTime dataCancelamento, string motivoCancelamento)
+		{
+			Id = Guid.NewGuid();
+			DataCriacao = DateTime.Now;			
+			UsuarioId = usuarioId;
+			Tipo = tipo;
+			Status = status;
+			DataAtivacao = dataAtivacao;
+			DataVencimento = dataVencimento;
+			DataCancelamento = dataCancelamento;
+			MotivoCancelamento = motivoCancelamento;
+			Validate(this, new AssinaturaValidator());
+		}
+
+		public Assinatura(Guid usuarioId, Guid id, AssinaturaTipoEnum tipo, AssinaturaStatusEnum status, DateTime dataCriacao, DateTime dataAtivacao, DateTime dataVencimento, DateTime dataCancelamento, string motivoCancelamento)
+		{			
+			Id = id;
+			UsuarioId = usuarioId;
+			Tipo = tipo;
+			Status = status;	
+			DataAtivacao = dataAtivacao;
+			DataVencimento = dataVencimento;
+			DataCancelamento = dataCancelamento;
+			MotivoCancelamento = motivoCancelamento;
+		}
+
+		public Guid UsuarioId { get; set; }
+		public virtual Usuario Usuario { get; set; }
+		public AssinaturaTipoEnum Tipo { get; set; }
+		public AssinaturaStatusEnum Status { get; set; }
+		public DateTime DataAtivacao { get; set; }
+		public DateTime DataVencimento { get; set; }
+		public DateTime DataCancelamento { get; set; }
+		public string MotivoCancelamento { get; set; }
+
+	}
+}
---
+++ OA_Core.Domain/Validations/AssinaturaValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using OA_Core.Domain.Entities;
+
+namespace OA_Core.Domain.Validations
+{
+	public class AssinaturaValidator : AbstractValidator<Assinatura>
+	{
+		public AssinaturaValidator()
+		{
+			RuleFor(a => a.UsuarioId)
+				.NotEmpty()
+				.WithMessage("É necessário usuarioId");
+		}
+	}
+}
