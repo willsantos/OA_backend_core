722990a1c1ba4fe27164996bf549c3da069a23f3
Alisson Demys <Alisson_demys@hotmail.com>
2023-09-26 00:13:17 -0300

    Finaliza camada de serviço para métodos solicitados

---
+++ OA_Core.Domain/Interfaces/Service/IUsuarioCursoService.cs
@@ -0,0 +1,12 @@
+﻿using OA_Core.Domain.Contracts.Request;
+using OA_Core.Domain.Contracts.Response;
+using OA_Core.Domain.Entities;
+
+namespace OA_Core.Domain.Interfaces.Service
+{
+	public interface IUsuarioCursoService
+	{
+		Task<Guid> PostUsuarioCursoAsync(UsuarioCursoRequest cursoRequest, Guid cursoId);
+		Task<List<CursoParaUsuarioResponse>> GetCursoDeUsuarioByIdAsync(Guid cursoId);
+	}
+}
--- OA_Core.Service/UsuarioCursoService.cs
+++ OA_Core.Service/UsuarioCursoService.cs
@@ -13,105 +13,64 @@ namespace OA_Core.Service
     public class UsuarioCursoService : IUsuarioCursoService
     {
         private readonly IMapper _mapper;
-        private readonly IUsuarioCursoRepository _cursoUsuarioRepository;
+        private readonly IUsuarioCursoRepository _usuarioCursoRepository;
         private readonly IUsuarioRepository _usuarioRepository;
 		private readonly ICursoRepository _cursoRepository;
 
 		private readonly INotificador _notificador;
 
-        public UsuarioCursoService(IMapper mapper, IUsuarioCursoRepository cursoUsuarioRepository, IUsuarioRepository usuarioRepository, ICursoRepository cursoRepository, INotificador notificador)
+        public UsuarioCursoService(IMapper mapper, IUsuarioCursoRepository usuarioCursoRepository, IUsuarioRepository usuarioRepository, ICursoRepository cursoRepository, INotificador notificador)
         {
             _mapper = mapper;
-            _cursoUsuarioRepository = cursoUsuarioRepository;
+            _usuarioCursoRepository = usuarioCursoRepository;
             _usuarioRepository = usuarioRepository;
 			_cursoRepository = cursoRepository;
             _notificador = notificador;
         }
 
-        public async Task DeleteUsuarioCursoAsync(Guid cursoId, Guid usuarioId)
-        {
-            var cursoUsuario = await _cursoUsuarioRepository.ObterAsync(x => x.CursoId == cursoId && x.UsuarioId == usuarioId) ??
-                throw new InformacaoException(StatusException.NaoEncontrado, $"UsuarioCurso não encontrado");			
-
-            cursoUsuario.DataDelecao = DateTime.Now;
-            await _cursoUsuarioRepository.EditarAsync(cursoUsuario);
-        }
-
-        public async Task<IEnumerable<UsuarioCurso>> GetAllUsuarioCursosAsync(int page, int rows)
-        {
-            var listEntity = await _cursoUsuarioRepository.ObterTodosAsync(page, rows);
-
-            return _mapper.Map<IEnumerable<UsuarioCurso>>(listEntity);
-        }
-
-        public async Task<UsuarioCurso> GetUsuarioCursoByIdAsync(Guid id)
-        {
-            var cursoUsuario = await _cursoUsuarioRepository.ObterPorIdAsync(id) ??
-                throw new InformacaoException(StatusException.NaoEncontrado, $"UsuarioCurso {id} não encontrado");
-
-            return _mapper.Map<UsuarioCurso>(cursoUsuario);
-        }
 
 		//retorna uma lista dos cursos que ele está inscrito, com o status e o progresso de cada um, exibindo o progresso como porcentagem.
-		//public async Task<List<UsuarioResponseComResponsavel>> GetUsuarioDeCursoByIdAsync(Guid cursoId)
-		//{
-		//	var curso = await _cursoRepository.ObterPorIdAsync(cursoId) ??
-		//		throw new InformacaoException(StatusException.NaoEncontrado, $"Curso {cursoId} não encontrado");
+		public async Task<List<CursoParaUsuarioResponse>> GetCursoDeUsuarioByIdAsync(Guid usuarioId)
+		{
+			var curso = await _usuarioRepository.ObterPorIdAsync(usuarioId) ??
+				throw new InformacaoException(StatusException.NaoEncontrado, $"Usuario {usuarioId} não encontrado");
 
-		//	var cursoUsuarioes = await _cursoUsuarioRepository.ObterTodosComIncludeAsync(x => x.CursoId == cursoId);
-		//	if (cursoUsuarioes.Count() <= 0)
-		//		throw new InformacaoException(StatusException.NaoEncontrado, $"UsuarioCurso {cursoId} não encontrado");
+			var usuarioCursos = await _usuarioCursoRepository.ObterTodosComIncludeAsync(x => x.UsuarioId == usuarioId);
+			if (usuarioCursos.Count() <= 0)
+				throw new InformacaoException(StatusException.NaoEncontrado, $"UsuarioCurso {usuarioId} não encontrado");
 
-		//	var usuarioes = new List<UsuarioResponseComResponsavel>();
+			var cursoList = new List<CursoParaUsuarioResponse>();
 
-		//	foreach( var usuario in cursoUsuarioes )
-		//	{
-		//		var response = _mapper.Map<UsuarioResponseComResponsavel>(usuario.Usuario);
-		//		response.Responsavel = usuario.Responsavel;
-		//		usuarioes.Add(response);
-		//	}
+			foreach (var cursos in usuarioCursos)
+			{
+				var response = _mapper.Map<CursoParaUsuarioResponse>(cursos.Curso);
+				response.Status = cursos.Status;
+				response.Progresso = cursos.Progresso;
+				cursoList.Add(response);
+			}
 
-		//	return usuarioes;
-		//}
+			return cursoList;
+		}
 
-		public async Task<Guid> PostUsuarioCursoAsync(UsuarioCursoRequest cursoUsuarioRequest, Guid cursoId)
+		public async Task<Guid> PostUsuarioCursoAsync(UsuarioCursoRequest usuarioCursoRequest, Guid usuarioId)
         {
-            var entity = _mapper.Map<UsuarioCurso>(cursoUsuarioRequest);
-			entity.CursoId = cursoId;
+            var entity = _mapper.Map<UsuarioCurso>(usuarioCursoRequest);
+			entity.UsuarioId = usuarioId;
 
+			if (await _usuarioRepository.ObterPorIdAsync(usuarioId) is null)
+                throw new InformacaoException(StatusException.NaoEncontrado, $"UsuarioId: {usuarioId} inválido ou não existente");
+			if (await _cursoRepository.ObterPorIdAsync(entity.CursoId) is null)
+				throw new InformacaoException(StatusException.NaoEncontrado, $"CursoId: {entity.CursoId} inválido ou não existente");
 
-			if (await _usuarioRepository.ObterPorIdAsync(cursoUsuarioRequest.UsuarioId) is null)
-                throw new InformacaoException(StatusException.NaoEncontrado, $"UsuarioId: {cursoUsuarioRequest.UsuarioId} inválido ou não existente");
-			if (await _cursoRepository.ObterPorIdAsync(cursoId) is null)
-				throw new InformacaoException(StatusException.NaoEncontrado, $"CursoId: {cursoId} inválido ou não existente");
+			if (await _usuarioCursoRepository.ObterAsync(x => x.UsuarioId == usuarioId && x.CursoId == entity.CursoId) is null)
+				throw new InformacaoException(StatusException.Conflito, $"Esse cadastro já foi realizado no banco");
 
 			if (!entity.Valid)
 				throw new InformacaoException(StatusException.FormatoIncorreto, $"{entity.ValidationResult}");
          
-            await _cursoUsuarioRepository.AdicionarAsync(entity);
+            await _usuarioCursoRepository.AdicionarAsync(entity);
             return entity.Id;
         }
 
-        public async Task PutUsuarioCursoAsync(Guid cursoId, UsuarioCursoRequest cursoUsuarioRequest)
-        {
-            var entity = _mapper.Map<UsuarioCurso>(cursoUsuarioRequest);   
-			entity.CursoId = cursoId;
-
-            if (!entity.Valid)
-            {
-                _notificador.Handle(entity.ValidationResult);
-                return;
-            }
-
-			var find = await _cursoUsuarioRepository.ObterAsync(x => x.CursoId == cursoId && x.UsuarioId == entity.UsuarioId) ??
-				throw new InformacaoException(StatusException.NaoEncontrado, $"UsuarioCurso não encontrado");
-
-			find.CursoId = entity.CursoId;
-			find.UsuarioId = entity.UsuarioId;
-			find.Responsavel = entity.Responsavel;
-            find.DataAlteracao = DateTime.Now;
-
-            await _cursoUsuarioRepository.EditarAsync(find);
-        }
     }
 }
