cfb82378068b89fa123f53a16299720cdbfac07b
Alisson Demys <Alisson_demys@hotmail.com>
2023-09-25 23:48:36 -0300

    Inicia implementação da relação de entidades

---
+++ OA_Core.Domain/Contracts/Request/UsuarioCursoRequest.cs
@@ -0,0 +1,12 @@
+﻿using OA_Core.Domain.Enums;
+
+namespace OA_Core.Domain.Contracts.Request
+{
+    public class UsuarioCursoRequest
+	{
+        public Guid CursoId { get; set; }
+		public StatusUsuarioCursoEnum Status { get; set; }
+		public int Progresso { get; set; }
+
+    }
+}
---
+++ OA_Core.Repository/Repositories/UsuarioCursoRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OA_Core.Domain.Entities;
+using OA_Core.Domain.Interfaces.Repository;
+using OA_Core.Repository.Context;
+using MySqlConnector;
+using System.Linq.Expressions;
+
+namespace OA_Core.Repository.Repositories
+{
+	public class UsuarioCursoRepository : BaseRepository<UsuarioCurso>, IUsuarioCursoRepository
+	{
+		private readonly CoreDbContext _context;
+
+		public UsuarioCursoRepository(CoreDbContext context) : base(context)
+		{
+			_context = context;
+		}
+
+		public async Task<IEnumerable<UsuarioCurso>> ObterTodosComIncludeAsync(Expression<Func<UsuarioCurso, bool>> expression)
+		{
+			return await _context.Set<UsuarioCurso>()
+				.Include(cp => cp.Curso)
+				.Where(expression)
+				.ToListAsync();
+		}
+	}
+}
