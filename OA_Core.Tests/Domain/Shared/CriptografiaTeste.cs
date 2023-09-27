b1920e7854dd3ab7ea9fb10e87e9a46957cdfa26
Edivandro <edileal1@gmail.com>
2023-09-27 14:37:52 -0300

    feat(criptografia): criptografia com bcrypt e testes ok

---
+++ OA_Core.Tests/Domain/Shared/CriptografiaTeste.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using OA_Core.Domain.Shared;
+
+namespace OA_Core.Tests.Domain.Shared
+{
+	[Trait("Domain", "Criptografia")]
+	public class CriptografiaTests
+	{
+		[Fact(DisplayName = "Criptografa senha")]
+		public void Criptografia_CriptografarSenha_DeveCriptografar()
+		{
+			// Arrange
+			string senha = "MinhaSenhaSecreta";
+
+			// Act
+			string senhaCriptografada = Criptografia.CriptografarSenha(senha);
+
+			// Assert
+			senhaCriptografada.Should().NotBeNullOrEmpty();
+			senhaCriptografada.Should().NotBe(senha);
+		}
+
+		[Fact(DisplayName = "Verifica senha")]
+		public void Criptografia_TestVerificarSenha_DeveVerificar()
+		{
+			// Arrange
+			string senha = "MinhaSenhaSecreta";
+			string senhaCriptografada = Criptografia.CriptografarSenha(senha);
+
+			// Act
+			bool senhaCorreta = Criptografia.VerificarSenha(senha, senhaCriptografada);
+			bool senhaIncorreta = Criptografia.VerificarSenha("SenhaIncorreta", senhaCriptografada);
+
+			// Assert
+			senhaCorreta.Should().BeTrue();
+			senhaIncorreta.Should().BeFalse();
+		}
+	}
+}
