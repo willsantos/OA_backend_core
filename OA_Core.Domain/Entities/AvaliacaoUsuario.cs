2a54bd63129747de5f4de86db70184845084b721
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-10-03 17:06:08 -0300

    :construction: feat/validacaoUsuario

--- OA_Core.Domain/Entities/AvaliacaoUsuario.cs
+++ OA_Core.Domain/Entities/AvaliacaoUsuario.cs
@@ -1,7 +1,32 @@
-﻿namespace OA_Core.Domain.Entities
+﻿using FluentValidation;
+using OA_Core.Domain.Validations;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OA_Core.Domain.Entities
 {
-	public class AvaliacaoUsuario
+	public class AvaliacaoUsuario 
 	{
+		public AvaliacaoUsuario(Guid avaliacaoId, 
+								Avaliacao avaliacao, 
+								Guid usuarioId, 
+								Usuario usuario, 
+								double? notaObtida, 
+								bool aprovado, 
+								DateTime inicio, 
+								DateTime? fim)
+		{
+			AvaliacaoId = avaliacaoId;
+			Avaliacao = avaliacao;
+			UsuarioId = usuarioId;
+			Usuario = usuario;
+			NotaObtida = notaObtida;
+			Aprovado = aprovado;
+			Inicio = inicio;
+			Fim = fim;
+			//Validate(this, new AvaliacaoUsuarioValidator());
+		}
+
 		public Guid AvaliacaoId{ get; set; }
 		public virtual Avaliacao Avaliacao { get; set; }
 		public Guid UsuarioId { get; set; }
@@ -10,5 +35,13 @@
 		public bool Aprovado { get; set; }
 		public DateTime Inicio { get; set; }
 		public DateTime? Fim { get; set; }
+		public bool Valid { get; set; }
+		public ValidationResult ValidationResult { get; set; }		
+
+		//public bool Validate<T>(T model, AbstractValidator<T> validator)
+		//{
+		//	ValidationResult = validator.Validate(model);
+		//	return Valid = ValidationResult.IsValid;
+		//}
 	}
 }
