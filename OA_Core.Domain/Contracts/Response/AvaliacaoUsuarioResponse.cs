0986687a4e087cc1296f9cc04017272851bf605d
Gabriela Calcabrine <gabrielacalcabrine@gmail.com>
2023-10-04 10:14:44 -0300

    :construction: feat/definindo chave primária

---
+++ OA_Core.Domain/Contracts/Response/AvaliacaoUsuarioResponse.cs
@@ -0,0 +1,8 @@
+﻿using OA_Core.Domain.Contracts.Request;
+
+namespace OA_Core.Domain.Contracts.Response
+{
+	public class AvaliacaoUsuarioResponse : AvaliacaoUsuarioRequest
+	{
+	}
+}
---
+++ OA_Core.Repository/Mappings/AvaliacaoUsuarioEntityMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OA_Core.Domain.Entities;
+
+namespace OA_Core.Repository.Mappings
+{
+	public class AvaliacaoUsuarioEntityMap
+	{
+		public void Configure(EntityTypeBuilder<AvaliacaoUsuario> builder)
+		{
+			//Ignora prop de validação
+			builder.Ignore(a => a.Valid).Ignore(a => a.ValidationResult);
+			builder.HasKey(a => new { a.AvaliacaoId, a.UsuarioId});		
+
+			//Mapeamento de relações
+			builder.HasOne(a => a.Usuario)
+				.WithMany()
+				.HasForeignKey(a => a.UsuarioId);
+			builder.HasOne(a => a.Avaliacao)
+				.WithMany()
+				.HasForeignKey(a => a.AvaliacaoId);
+		}
+	}
+}
