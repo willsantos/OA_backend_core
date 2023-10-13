126fc41e294da370a0db2ceda9ace665caab63ab
Alisson Demys <Alisson_demys@hotmail.com>
2023-10-13 12:14:18 -0300

    Modela entidade de relacionamento

---
+++ OA_Core.Repository/Mappings/UsuarioAulaEntityMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OA_Core.Domain.Entities;
+
+namespace OA_Core.Repository.Mappings
+{
+	public class UsuarioAulaEntityMap
+    {
+        public void Configure(EntityTypeBuilder<UsuarioAula> builder)
+        {
+			//Mapeamento de relações
+			builder.HasOne(cp => cp.Aula)
+				.WithMany(c => c.UsuarioAulas)
+				.HasForeignKey(cp => cp.AulaId);
+
+			builder.HasOne(cp => cp.Usuario)
+				.WithMany(p => p.UsuarioAulas)
+				.HasForeignKey(cp => cp.UsuarioId);
+
+			builder.HasKey(c => new { c.AulaId, c.UsuarioId });
+		}
+	}
+}
